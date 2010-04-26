adcd1ab261f5595e0d7f1e6b383465c131ef41d8
Rafael Baboni Dominiquini <rafaeldominiquini@gmail.com>
2010-04-26 19:48:45 -0300

    Novos scripts.

--- C++/scripts/exec.sh
+++ C++/scripts/exec.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+#Executa os testes la$1.prb ate la$2.prb, $3 vezes cada
 
 exec="./Ateams"
 
-for((i = $1; i <= $2; i++));
+for((j = 0; j < $3; j++));
 do
-    if [ $i -le 9 ];
-    then
-        $exec -i dados/la0$i.prb $4 $5 $6 $7 $8 $9
-    else
-        $exec -i dados/la$i.prb $4 $5 $6 $7 $8 $9
-    fi
+    for((i = $1; i <= $2; i++));
+    do
+        if [ $i -le 9 ];
+        then
+            $exec -i dados/la0$i.prb $4 $5 $6 $7 $8 $9
+        else
+            $exec -i dados/la$i.prb $4 $5 $6 $7 $8 $9
+        fi
+    done
 done
