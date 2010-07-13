b07096622621d3410adc6ba197b4cab335197706
Rafael Baboni Dominiquini <rafaeldominiquini@gmail.com>
2010-07-13 02:34:17 -0300

    Novo script: exec.sh

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
-#Executa os testes la$1.prb ate la$2.prb, $3 vezes cada
 
 exec="./Ateams"
+num=$#
+rep=`eval echo '${'$num'}'`
 
-if [ $1 -le $2 ];
+
+echo
+
+
+if [ -e $rep ]
 then
-    for((j = 0; j < $3; j++));
-    do
-        for((i = $1; i <= $2; i++));
-        do
-            if [ $i -le 9 ];
-            then
-                echo "$exec -i dados/la0$i.prb $4 $5 $6 $7 $8 $9"
-                $exec -i dados/la0$i.prb $4 $5 $6 $7 $8 $9
-            else
-                echo "$exec -i dados/la$i.prb $4 $5 $6 $7 $8 $9"
-                $exec -i dados/la$i.prb $4 $5 $6 $7 $8 $9
-            fi
-        done
-    done
+    rep=1
 else
-    for((j = 0; j < $3; j++));
-    do
-        for((i = $1; i >= $2; i--));
-        do
-            if [ $i -le 9 ];
-            then
-                echo "$exec -i dados/la0$i.prb $4 $5 $6 $7 $8 $9"
-                $exec -i dados/la0$i.prb $4 $5 $6 $7 $8 $9
-            else
-                echo "$exec -i dados/la$i.prb $4 $5 $6 $7 $8 $9"
-                $exec -i dados/la$i.prb $4 $5 $6 $7 $8 $9
-            fi
-        done
-    done
+    num=$(($num-1))
 fi
+
+
+echo "Number Of Instances: $num"
+echo "Repetitions: $rep"
+
+echo
+eval "make install"
+echo
+
+
+for((i = 1; i <= $num; i++));
+do
+    dado='${'$i'}'
+
+    for((j = 0; j < $rep; j++));
+    do
+        eval "echo Running: $dado"
+        eval "$exec -i $dado"
+    done
+done
