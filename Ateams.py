c2c64333d90b4d3bd0baaf5ee02be977870acc8c
Rafael Baboni Dominiquini <rafaeldominiquini@gmail.com>
2021-12-25 15:25:24 -0300

    Ajustes no Launcher

--- Ateams.py
+++ Ateams.py
@@ -8,12 +8,21 @@ import timeit
 import time
 import math
 import os
+
+ATEAMS_PATH_PLACEHOLDER = "#ATEAMS_PATH#"
     
 ALGORITHMS = ['BinPacking', 'FlowShop', 'GraphColoring', 'JobShop', 'KnapSack', 'TravellingSalesman']
 
-DEFAULT_PARAM = f"{os.getcwd()}/Ateams_Base/parameters/DEFAULT.xml"
+BINARIES = {A: f"{ATEAMS_PATH_PLACEHOLDER}/{A}/bin/{A}" for A in ALGORITHMS}
 
-DEFAULT_INPUTS = {A: f"{os.getcwd()}/{A}/input/{I}" for (A, I) in zip(ALGORITHMS, ['binpack1_01.prb', 'car1.prb', 'jean.prb', 'la01.prb', 'mk_gk01.prb', 'br17.prb'])}
+DEFAULT_PARAM = f"{ATEAMS_PATH_PLACEHOLDER}/Ateams_Base/parameters/DEFAULT.xml"
+
+DEFAULT_INPUTS = {A: f"{ATEAMS_PATH_PLACEHOLDER}/{A}/input/{I}" for (A, I) in zip(ALGORITHMS, ['binpack1_01.prb', 'car1.prb', 'jean.prb', 'la01.prb', 'mk_gk01.prb', 'br17.prb'])}
+
+DEFAULT_OUTPUTS_PATH = {A: f"{ATEAMS_PATH_PLACEHOLDER}/{A}/results/" for A in ALGORITHMS}
+
+def ateams_path(path=ATEAMS_PATH_PLACEHOLDER, placeholder=ATEAMS_PATH_PLACEHOLDER):
+    return path.replace(placeholder, os.path.dirname(os.path.abspath(__file__)))
 
 def validate_path(ctx: click.core.Context=None, param: click.core.Option=None, value: click.Path=None):
     try:
@@ -23,8 +32,8 @@ def validate_path(ctx: click.core.Context=None, param: click.core.Option=None, v
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('-a', '--algorithm', type=click.Choice(ALGORITHMS, case_sensitive=False), required=True, prompt="Algorithm: ", help='Algorithm')
-@click.option('-p', '--parameters', type=click.Path(exists=True, dir_okay=False, file_okay=True), required=False, default=DEFAULT_PARAM, callback=validate_path, help='Input Parameters File')
-@click.option('-i', '--input', type=click.Path(exists=False, dir_okay=False, file_okay=True), required=False, default=None, callback=validate_path, help='Input Data File')
+@click.option('-p', '--parameters', type=click.Path(exists=True, dir_okay=False, file_okay=True), required=False, callback=validate_path, help='Input Parameters File')
+@click.option('-i', '--input', type=click.Path(exists=True, dir_okay=False, file_okay=True), required=False, callback=validate_path, help='Input Data File')
 @click.option('-r', '--result', type=click.Path(exists=False, dir_okay=False, file_okay=True), required=False, callback=validate_path, help='Output Result File')
 @click.option('-t', '--pop', type=click.Path(exists=False, dir_okay=False, file_okay=True), required=False, callback=validate_path, help='Population File')
 @click.option('-o', '--write_output', type=click.BOOL, is_flag=True, help='Force Write Output Files')
@@ -38,14 +47,13 @@ def ateams(algorithm, parameters, input, result, pop, write_output, show_info, s
 
         return math.trunc(number) if decimals <= 0 else math.trunc(number * factor) / factor
 
-    def __evaluate_output_files(parameter):
+    def __evaluate_output_files(extension):
         filename = pathlib.Path(input).stem
-        extension = "res" if parameter == "result" else "pop"
 
-        return validate_path(value=f"{os.getcwd()}/{algorithm}/results/{filename}.{extension}")
-
+        return validate_path(value=ateams_path(DEFAULT_OUTPUTS_PATH[algorithm] + filename + '.' + extension))
+    
     def __build_command_line():       
-        command_line = f"{os.getcwd()}/{algorithm}/bin/{algorithm}"
+        command_line = BINARIES[algorithm]
 
         if(parameters is not None): command_line += f" -p {parameters}"
         
@@ -59,7 +67,7 @@ def ateams(algorithm, parameters, input, result, pop, write_output, show_info, s
 
         if(show_solution): command_line += f" -s"
 
-        return command_line
+        return ateams_path(command_line)
 
     def __execute_ateams(cmd, repeat=1):
         return timeit.repeat(stmt=lambda: os.system(cmd), repeat=repeat, number=1)
@@ -67,7 +75,7 @@ def ateams(algorithm, parameters, input, result, pop, write_output, show_info, s
     algorithm = next((a for a in ALGORITHMS if a.casefold() == algorithm.casefold()), algorithm)
     if(parameters is None): parameters = DEFAULT_PARAM
     if(input is None): input = DEFAULT_INPUTS[algorithm]
-    if(result is None and write_output): result = __evaluate_output_files("result")
+    if(result is None and write_output): result = __evaluate_output_files("res")
     if(pop is None and write_output): pop = __evaluate_output_files("pop")
 
     command_line = __build_command_line()
